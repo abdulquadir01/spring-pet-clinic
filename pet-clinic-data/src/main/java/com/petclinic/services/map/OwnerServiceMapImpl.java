5fd0c662131b00c646cecfc4ae9399de709cb5af
abdulquadir01 <abdulquadir01@outlook.com>
2022-05-19 14:53:17 +0530

    refactored MapImpl to auto-generate Id property. closes #23

--- pet-clinic-data/src/main/java/com/petclinic/services/map/OwnerServiceMapImpl.java
+++ pet-clinic-data/src/main/java/com/petclinic/services/map/OwnerServiceMapImpl.java
@@ -20,7 +20,7 @@ public class OwnerServiceMapImpl extends AbstractMapService<Owner, Long> impleme
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/petclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jhon");
         owner1.setLastName("Cena");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Triple");
         owner2.setLastName("H");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners Loaded...");
 
         Vet vet1= new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Dr");
         vet1.setLastName("Banner");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Dr");
         vet2.setLastName("Doom");
 
