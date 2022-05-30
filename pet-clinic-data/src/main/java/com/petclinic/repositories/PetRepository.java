97de88eb96245b73853fd09b0f22e1dce48700b7
abdulquadir01 <abdulquadir01@outlook.com>
2022-05-31 04:19:29 +0530

    added JPA repositories. closes #39

---
+++ pet-clinic-data/src/main/java/com/petclinic/repositories/PetRepository.java
@@ -0,0 +1,8 @@
+package com.petclinic.repositories;
+
+import com.petclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepository extends CrudRepository<Pet, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/petclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,8 @@
+package com.petclinic.repositories;
+
+import com.petclinic.model.Specialty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+
+}
