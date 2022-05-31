d0564438e845acefa02d68ab395462e3f91a1130
abdulquadir01 <abdulquadir01@outlook.com>
2022-05-31 06:08:34 +0530

    added springDataJPA service for specialty. closes #44

---
+++ pet-clinic-data/src/main/java/com/petclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package com.petclinic.services.springdatajpa;
+
+import com.petclinic.model.Specialty;
+import com.petclinic.repositories.SpecialtyRepository;
+import com.petclinic.services.SpecialtyService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpecialtySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+
+
+}
