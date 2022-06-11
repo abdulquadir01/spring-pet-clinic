4dab567db5c71aceff0efc98cdb0d888064630f8
abdulquadir01 <abdulquadir01@outlook.com>
2022-06-12 05:10:55 +0530

    adding circleci config file.

--- pet-clinic-web/src/test/java/com/petclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/petclinic/SpringPetClinicApplicationTests.java
@@ -1,12 +1,17 @@
 package com.petclinic;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 //JUnit5 syntax included for this test.
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//public class SpringPetClinicApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
