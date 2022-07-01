7d5ae8330a716a96ba26454f60b7153de9625b54
abdulquadir01 <abdulquadir01@outlook.com>
2022-07-02 04:43:57 +0530

    implemented find owner functionality. closes #54

--- pet-clinic-data/src/main/java/com/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/petclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package com.petclinic.services;
 
 import com.petclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
--- pet-clinic-data/src/main/java/com/petclinic/services/map/OwnerServiceMapImpl.java
+++ pet-clinic-data/src/main/java/com/petclinic/services/map/OwnerServiceMapImpl.java
@@ -8,6 +8,7 @@ import com.petclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 
@@ -81,5 +82,10 @@ public class OwnerServiceMapImpl extends AbstractMapService<Owner, Long> impleme
                 .filter( owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        return null;
     }
 }
